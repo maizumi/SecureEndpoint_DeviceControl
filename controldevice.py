4e62c09b9178a00b6a3d531ff4275578ca7b4e54
maizumi <maizumi@cisco.com>
2022-12-31 19:11:07 +0900

    Update controldevice.py

--- controldevice.py
+++ controldevice.py
@@ -1,44 +1,40 @@
 import requests
 import csv
 import json
-from requests.auth import HTTPBasicAuth
-from getpass import getpass
-import os
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session
-from werkzeug.utils import secure_filename
-
+import time
 
 base_url = "https://api.apjc.amp.cisco.com/v3/"
 url = "https://visibility.apjc.amp.cisco.com/iroh/oauth2/token"
 
 payload = "grant_type=client_credentials"
+#authorization : Basic の後ろにはSecureXで発行したAPI Client IDとPasswordをBase64に変換して記入（例：Client IDがclient-d3sd4dg5-3f9e-4hds-8adw-d08a5d8e8hds, Client passwordがJv6cjPQDQni2KD9sr7IFHqxZ2_iH6ceDLSSEBWx6pmsL5Am6ZxzTXQの場合はIDとPasswordをコロン(:)で繋いで"client-d3sd4dg5-3f9e-4hds-8adw-d08a5d8e8hds:Jv6cjPQDQni2KD9sr7IFHqxZ2_iH6ceDLSSEBWx6pmsL5Am6ZxzTXQ"の形にした後にBase64に変換して＞”Y2xpZW50LWQzc2Q0ZGc1LTNmOWUtNGhkcy04YWR3LWQwOGE1ZDhlOGhkczpKdjZjalBRRFFuaTJLRDlzcjdJRkhxeFoyX2lINmNlRExTU0VCV3g2cG1zTDVBbTZaeHpUWFE=”とし、それを'authorization': "Basicの後に貼り付ける
 headers = {
     'content-type': "application/x-www-form-urlencoded",
     'accept': "application/json",
-    'authorization': "Basic Y2xpZW50LWYwYmNiNWRiLWE5OWUtNGJkYy04YTQ3LWQwOGE1ZDhlOGFiYTpKdjZjakZZTlFuaTJPRzlzcjdJRkhxeFoyX2lINmNlUENOTkVCV3gycG1zVjZBbTZaeHpUWFE=",
+    'authorization': "Basic Y2xpZW50LWYwYmNiNWRiLWE5OWUtNGJkYy04YTQ3LWQwOGE1ZDhlOGFiYTpKdjZjakZZTlFuaTJPRzlzcjdJRkhxeFoyX2lINmNlUENOTkVCV3gycG1zVjZBbTZaeHpUWFE=", 
     'cache-control': "no-cache",
     }
 
 response = requests.request("POST", url, data=payload, headers=headers)
+#print(response.text)
+time.sleep(0.2)
 
-
-print(response.text)
-
+#SecureX API アクセストークン
 accesstoken = response.json()["access_token"]
-
-print(accesstoken)
+#print(accesstoken)
 
 url2 = base_url+"access_tokens"
-
 header4endpoint = {
     "Authorization": "Bearer " + accesstoken
     }
-
 response_accesstoken = requests.request("POST", url2, headers=header4endpoint)
-print(response_accesstoken.json())
+#print(response_accesstoken.json())
 
+#Secure Endpoint API アクセストークン
 EndpointAccessToken = response_accesstoken.json()["access_token"]
-print(EndpointAccessToken)
+#print(EndpointAccessToken)
+
+time.sleep(0.2)
 
 url3 = base_url+"organizations?size=10"
 
@@ -47,23 +43,22 @@ header4org = {
     }
 
 response_org = requests.request("GET", url3, headers=header4org)
-print(response_org.json())
+#print(response_org.json())
 
 orgData = response_org.json() 
 
-print(orgData["data"][0]["organizationIdentifier"])
+#print(orgData["data"][0]["organizationIdentifier"])
 orgID = orgData["data"][0]["organizationIdentifier"]
 
 url4 = base_url+"organizations/"+orgID+"/device_control/configurations?size=10"
 response_devices = requests.request("GET", url4, headers=header4org)
 
-print(response_devices.text)
+#print(response_devices.text)
 configGuidData = response_devices.json() 
-configGuid = configGuidData["data"][0]["guid"]
 #Management > Device Control のNameでアルファベット順で上記[data]と[guid]番号が変わるので、予め追加したいDeviceControl Config画面で何番目のconfigに追加したいか確認してから番号を変更・入力
+configGuid = configGuidData["data"][0]["guid"]
 
 url6 = base_url+"organizations/"+orgID+"/device_control/configurations/"+configGuid+"/rules"
-
 
 header4device = {
     "Authorization": "Bearer " + EndpointAccessToken,
@@ -71,8 +66,10 @@ header4device = {
     'accept': "application/json"
     }
 
-
 result = []
+rulecount = 1
+
+#with open("")にcsvファイルのpathを記入
 with open('/Users/maizumi/Documents/15.Technology/Programming/SecureEndpoint/rules.csv', encoding='utf-8-sig') as f:
 	reader = csv.DictReader(f)
 	for r in reader:
@@ -94,11 +91,12 @@ with open('/Users/maizumi/Documents/15.Technology/Programming/SecureEndpoint/rul
 		json_result = json.dumps(result)
 		lst_str = str(json_result)[1:-1]
 		response_rules = requests.request("POST", url6, data=lst_str, headers=header4device)
-		print(response_rules)
-		time.sleep(1)
+		#print(response_rules)
+		if response_rules.status_code == 200 or response_rules.status_code == 201:
+			print("row number "+str(rulecount)+" has added successfully")
+		else:
+			print("row number "+str(rulecount)+" has something wrong with value in CSV file")
+		time.sleep(0.1)
 		result.clear()
+		rulecount += 1
 	
-
-
-
-
